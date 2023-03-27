2cdc48f95c81e40bf10e189670b73912ecef40a2
Andre Piona <alpiona@hotmail.com>
2023-03-27 17:05:20 -0300

    Fix buttons size in side navbar

--- src/components/SideNavbar/SideNavbar.tsx
+++ src/components/SideNavbar/SideNavbar.tsx
@@ -1,5 +1,7 @@
 import { Box, Button, Icon, Link, Text, VStack } from "@chakra-ui/react";
 import NextLink from "next/link";
+import { BsCardChecklist } from "react-icons/bs";
+import { CgNotes } from "react-icons/cg";
 import { MdAttachMoney } from "react-icons/md";
 
 const menuOptions = [
@@ -7,12 +9,25 @@ const menuOptions = [
     title: "Bills",
     icon: MdAttachMoney,
     redirect: "/bills",
+    isActive: true,
+  },
+  {
+    title: "Notes",
+    icon: CgNotes,
+    redirect: "/notes",
+    isActive: false,
+  },
+  {
+    title: "Checklist",
+    icon: BsCardChecklist,
+    redirect: "/check-lists",
+    isActive: false,
   },
 ];
 
 const SideNavbar: React.FC = () => {
   return (
-    <Box position="fixed" bg="gray.600" width="10%" height="100%">
+    <Box position="fixed" bg="gray.600" width="10%" minWidth={30} height="100%">
       <VStack spacing="20px" align="center">
         {menuOptions.map((mo) => (
           <Link key={mo.title} as={NextLink} href={mo.redirect}>
@@ -22,13 +37,14 @@ const SideNavbar: React.FC = () => {
               flexDirection="column"
               mt={4}
               height={14}
-              size="lg"
               bgColor="gray.200"
               borderColor="gray.500"
               borderWidth={3}
             >
               <Icon boxSize={7} as={mo.icon} color="black" />
-              <Text as="b">{mo.title}</Text>
+              <Text as="b" width={20}>
+                {mo.title}
+              </Text>
             </Button>
           </Link>
         ))}
