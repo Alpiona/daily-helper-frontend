b7c36d90e68ee0539ff00f581c8d3c10929f600f
Andre Piona <alpiona@hotmail.com>
2023-03-27 13:32:07 -0300

    Add SideNavbar

--- src/components/SideNavbar/SideNavbar.tsx
+++ src/components/SideNavbar/SideNavbar.tsx
@@ -1,14 +1,37 @@
-import { Box, Text, VStack } from "@chakra-ui/react";
-import React from "react";
+import { Box, Button, Icon, Link, Text, VStack } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { MdAttachMoney } from "react-icons/md";
 
-type SideNavbarProps = {};
+const menuOptions = [
+  {
+    title: "Bills",
+    icon: MdAttachMoney,
+    redirect: "/bills",
+  },
+];
 
-const SideNavbar: React.FC<SideNavbarProps> = () => {
+const SideNavbar: React.FC = () => {
   return (
     <Box position="fixed" bg="gray.600" width="10%" height="100%">
-      <VStack height="100%" justifyContent="center" spacing="20px">
-        <Text>Teste</Text>
-        <Text>Teste</Text>
+      <VStack spacing="20px" align="center">
+        {menuOptions.map((mo) => (
+          <Link key={mo.title} as={NextLink} href={mo.redirect}>
+            <Button
+              rounded={10}
+              key={mo.title}
+              flexDirection="column"
+              mt={4}
+              height={14}
+              size="lg"
+              bgColor="gray.200"
+              borderColor="gray.500"
+              borderWidth={3}
+            >
+              <Icon boxSize={7} as={mo.icon} color="black" />
+              <Text as="b">{mo.title}</Text>
+            </Button>
+          </Link>
+        ))}
       </VStack>
     </Box>
   );
